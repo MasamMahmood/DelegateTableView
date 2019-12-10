2d36b20fd02692b20f0112d4fd3f89a1d4d33814
“Masam <masam.mahmood@tekhnelogos.com>
2019-12-10 11:37:52 +0300

    Initial Commit

---
+++ DelegateTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateTableView
+//
+//  Created by Emre Türker on 10.12.2019.
+//  Copyright © 2019 MasamMahmood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
